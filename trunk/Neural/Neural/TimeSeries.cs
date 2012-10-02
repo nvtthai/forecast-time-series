922451a54cc9bff48f2fe9b7fd7d44d55ef74157
tranthesi1990@gmail.com <tranthesi1990@gmail.com@d76e1fa2-2ec1-1278-d2cf-127c67915ff9>
2012-10-02 17:51:37 +0000

    check, fix bug on Neural Change model use homology -> replace bias by a node

---
+++ trunk/Neural/Neural/TimeSeries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class TimeSeries
+    {
+        public string fileName { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+
+        public List<double> originalSeries;
+        public List<double> arimaProcessedSeries;
+        public List<double> arimaErrorSeries;
+        public List<double> neuralTrainSeries;
+        public List<double> neuralValidateSeries;
+        public List<double> neuralTestSeries;
+
+    }
+}
