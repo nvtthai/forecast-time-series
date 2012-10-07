59524a305952f52aa0404812e0609aec64f38b43
tranthesi1990 <tranthesi1990@gmail.com>
2012-10-07 14:02:12 +0000

    add backup code

---
+++ AutoARIMA/AutoARIMA/Plot_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoARIMA
+{
+    public partial class Plot_Form : Form
+    {
+        public Plot_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
